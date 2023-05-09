0fe43ebc032ad6023cbd6fe633feaab1e923b479
Jacob <jacobsimoninielsen@gmail.com>
2023-05-09 14:53:57 +0200

    form

---
+++ simpelrouter/src/components/hooks/hook.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+
+export const useFetch = (url, key) => {
+    const [apiData, setApiData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        const fetchData = async () = {
+            
+        }
+    })
+
+  return ( apiData)
+}
--- simpelrouter/src/components/pages/home.jsx
+++ simpelrouter/src/components/pages/home.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Home = () => {
+
+  const intialState = {
+    name: "",
+    lastname: "",
+    address: "",
+    city: "",
+    zipcode: "",
+  };
+
+  const [state, setState] = useState(intialState)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setState((prev) => ({ ...prev, [name]: value}));
+  };
+
+  const { name, lastname, address, city, zipcode } = state;
+
   return (
-    <h1>Home</h1>
+    <>
+      <input type="text" name="name" value={name} onChange={handleChange} placeholder='enter name' /> <br />
+      <input type="text" name="lastname" value={lastname} onChange={handleChange} placeholder='enter lastname' /> <br />
+      <input type="text" name="address" value={address} onChange={handleChange} placeholder='enter address' /> <br />
+      <input type="text" name="city" value={city} onChange={handleChange} placeholder='enter city' /> <br />
+      <input type="number" name="zipcode" value={zipcode} onChange={handleChange} placeholder='enter zipcode' /> <br />
+    
+      Firstname: {name} <br />
+      Lastname: {lastname} <br />
+      Address: {address} <br />
+      City: {city} <br />
+      Zipcode: {zipcode} <br />
+      <button onClick={() => setState(intialState)}>Reset</button>
+    </>
   )
 }
